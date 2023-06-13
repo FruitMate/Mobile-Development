f6358e7bf03b1be3b90ae0786c0d0ca41750cb5f
Muhammad Rafif Baihaqi <70611863+Raff-28@users.noreply.github.com>
2023-06-13 20:31:46 +0700

    Revert "Penambahan History Update"

--- app/src/main/java/com/capstone/tesfirebase/data/response/HistoryResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.capstone.tesfirebase.data.response
-
-
-data class HistoryResponse(
-    val uid: String,
-    val classification_result: String,
-    val timestamp: String,
-    val image_url: String
-)
